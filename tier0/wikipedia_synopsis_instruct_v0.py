f4d0588a334b24d8c9738b55fe87a4e9872f798a
Baked Pineapple <bakedpineapple@airmail.cc>
2023-07-05 22:09:19 -0700

    oops

--- tier0/wikipedia_synopsis_instruct_v0.py
+++ tier0/wikipedia_synopsis_instruct_v0.py
@@ -30,8 +30,8 @@ INSTRUCT_TEMPLATES = [
 ]
 PRE_RESPONSE_TEMPLATES = [
     "Here is a summary for the episode {episode}"
-    " of {mlp}, which is the {ep}th episode of the {s}th season: ",
-    "I can provide a summary of the episode {episode} from {mlp},"
+    " of {mlp}, which is the {ep_th} episode of the {s_th} season: ",
+    "Of course. I can provide a summary of the episode {episode} from {mlp},"
     "which is the {ep_th} episode of the {s_th} season: " # idk
 ]
 
@@ -39,8 +39,10 @@ def gen_summary_question(season_num, episode_title, episode_num):
     temp = random.choice(INSTRUCT_TEMPLATES)
     return temp.format(episode = episode_title,
         mlp = instruct_util.choose_mlp(),
-        ep_th = instruct_util.choose_nth(int(episode_num)), ep = episode_num,
-        s_th = instruct_util.choose_nth(int(season_num)), s = season_num)
+        ep_th = instruct_util.choose_nth(int(episode_num)),
+        ep = int(episode_num),
+        s_th = instruct_util.choose_nth(int(season_num)),
+        s = int(season_num))
 
 with open(LOAD_FILE,'r') as f:
     wiki_synopsis_list = json.loads(f.read())
@@ -51,8 +53,16 @@ for s in range(1,3):
     for ep,x in enumerate(wiki_synopsis_list[idx]):
         instruction = gen_summary_question(
             s, x["title"], x["subseason_ep_number"])
+        pre_response = random.choice(PRE_RESPONSE_TEMPLATES).format(
+            episode = x["title"],
+            mlp = instruct_util.choose_mlp(),
+            ep_th = instruct_util.int_to_written(
+                int(x["subseason_ep_number"])),
+            ep = x["subseason_ep_number"],
+            s_th = instruct_util.int_to_written(int(s)),
+            s = s)
         instruct_dataset.append({"instruction": instruction,
-            "input": "", "output": random.choice(PRE_RESPONSE_TEMPLATES)
+            "input": "", "output": pre_response
             +x["synopsis"]})
 
 with open(SAVE_FILE,'w') as f:
