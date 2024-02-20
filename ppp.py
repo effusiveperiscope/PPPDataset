84c3d6f73bc07d962896f4ce4679e4c6ea7d0f28
Baked Pineapple <bakedpineapple@airmail.cc>
2024-02-20 01:25:13 -0800

    label generation for resorting?

--- ppp.py
+++ ppp.py
@@ -19,6 +19,46 @@ from tqdm import tqdm
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Unfinished: label mappings for the label files to locations in the master files
+# Unfinished because I don't actually have the original audios for most of these
+SPECIAL_LABEL_MAPPINGS = {
+    'mobile game_aj': 'MASTER_FILE_2/Other sources/Mobile Game/Applejack',
+    'mobile game_fs': 'MASTER_FILE_2/Other sources/Mobile Game/Fluttershy',
+    'mobile game_nmm': 'MASTER_FILE_2/Other sources/Mobile Game/Nightmare Moon',
+    'mobile game_pp': 'MASTER_FILE_2/Other sources/Mobile Game/Pinkie Pie',
+    'mobile game_ra': 'MASTER_FILE_2/Other sources/Mobile Game/Rarity',
+    'mobile game_rd': 'MASTER_FILE_2/Other sources/Mobile Game/Rainbow Dash',
+    'mobile game_spike': 'MASTER_FILE_2/Other sources/Mobile Game/Spike',
+    'songs': 'MASTER_FILE_2/Songs',
+    'eqg_dance magic': 'MASTER_FILE_1/Sliced Dialogue/EQG/EQG Dance Magic',
+    'eqg_forgotten friendship': 'MASTER_FILE_1/Sliced Dialogue/EQG/EQG Forgotten Friendship',
+    'eqg_friendship_games': 'MASTER_FILE_1/Sliced Dialogue/EQG/EQG Friendship Games',
+    'eqg_original_movie': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Original',
+    'eqg_legend_of_everfree': 'MASTER_FILE_1/Sliced Dialogue/EQG/EQG Legend of Everfree',
+    'eqg_mirror magic': 'MASTER_FILE_1/Sliced Dialogue/EQG/EQG Mirror Magic',
+    'eqg_movie magic': 'MASTER_FILE_1/Sliced Dialogue/EQG/EQG Movie Magic',
+    'eqg_holidays unwrapped': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Holidays Unwrapped',
+    'eqg_original_movie_special source': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Original',
+    'eqg_rainbow rocks_special source': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Rainbow Rocks',
+    'eqg_rollercoaster of friendship': 'MASTER_FILE_1/Sliced Dialogue/EQG/EQG Roller Coaster of Friendship',
+    'eqg_rollercoaster of friendship_special source': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Roller Coaster of Freindship Special Source',
+    'eqg_better together_s02e04': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Shorts/Better Together/S2/s2e4_Street Magic With Trixie',
+    'eqg_better together_s02e05': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Shorts/Better Together/S2/s2e5_Sic Skateboard',
+    'eqg_better together_s02e06': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Shorts/Better Together/S2/s2e6_Street Chic',
+    'eqg_better together_s02e07': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Shorts/Better Together/S2/s2e7_Game Stream',
+    'eqg_better together_s02e08': 'MASTER_FILE_1/Sliced Dialogue/Special source/EQG Shorts/Better Together/S2/s2e8_Best in Show The Preshow',
+    'fim_s09e23_special source': 'MASTER_FILE_1/Sliced Dialogue/Special source/s9e23 [CAUTION - REVERB]',
+    'fim_s09e25_special source': 'MASTER_FILE_1/Sliced Dialogue/Special source/s9e25 [CAUTION - REVERB]',
+    'fim_s09e26_special source': 'MASTER_FILE_1/Sliced Dialogue/Special source/s9e26 [CAUTION - REVERB]',
+    'fim_movie': "MASTER_FILE_2/MLP Movie (still has music, don't use this in any training)",
+    'fim_rainbow roadtrip': "MASTER_FILE_1/Sliced Dialogue/FiM/Rainbow Roadtrip",
+}
+SPECIAL_LABEL_MAPPINGS_SPECIFIER = {
+    'fim_s09e23_special source': 's9e23',
+    'fim_s09e25_special source': 's9e25',
+    'fim_s09e26_special source': 's9e26'
+}
+
 # Remove nums from ARPAbet dictionary
 def load_dictionary(dict_path, remove_nums=True):
     arpadict = dict()
@@ -91,7 +131,8 @@ class PPPDataset:
             no_parse = False,
             audio_input_format = '.flac',
             force_character = '',
-            emotions : list = []):
+            emotions : list = [],
+            do_rev_index = False):
         dataset = PPPDataset()
 
         if len(characters):
@@ -99,10 +140,14 @@ class PPPDataset:
         else:
             print(f"Collecting data for all characters")
 
+        if do_rev_index:
+            assert not ignore_text
+
         for (root,_,files) in tqdm(os.walk(sliced_dialogue)):
             for f in files:
                 if not f.endswith(audio_input_format):
                     continue
+                f_basename = f.removesuffix('.flac')
                 f = os.path.join(root,f)
 
                 if no_parse:
@@ -111,7 +156,7 @@ class PPPDataset:
                     parse['emotion'] = ''
                     parse['txt'] = ''
                     parse['line'] = ''
-                    parse['noise'] = 'Clean'
+                    parse['noise'] = ''
                     parse['file'] = os.path.abspath(f)
                 else:
                     parse = PPPDataset.character_parse(f)
@@ -128,14 +173,18 @@ class PPPDataset:
                     elif max_noise == 1:
                         if parse['noise'] == 'Very Noisy':
                             continue
+                    elif max_noise == -1:
+                        if parse['noise'] in ['Clean','Noisy']:
+                            continue
                     parse['file'] = os.path.abspath(f)
                     if not ignore_text:
                         txt = str(Path(f.removesuffix('..flac').removesuffix('.flac')
                             ))+'.txt'
                         if not os.path.exists(txt):
-                            print(txt)
-                            txt = txt[:-4]+'..txt'
-                            print(txt)
+                            if os.path.exists(txt[:-4]+'..txt'):
+                                txt = txt[:-4]+'..txt'
+                            elif os.path.exists(txt[:-5]+'.txt'):
+                                txt = txt[:-5]+'.txt'
                             assert os.path.exists(txt) 
                         parse['txt'] = os.path.abspath(txt)
                         with open(parse['txt'], 'r', encoding='utf8') as f:
@@ -151,6 +200,101 @@ class PPPDataset:
             print(f"Finished collecting data for all characters")
         return dataset
 
+    def lookup_by_substr(self, st):
+        return [x for x in self.file_dict.keys() if st in x]
+
+    def label_mapping(label_basename):
+        if label_basename in SPECIAL_LABEL_MAPPINGS:
+            return (SPECIAL_LABEL_MAPPINGS[label_basename], 
+                SPECIAL_LABEL_MAPPINGS_SPECIFIER.get(label_basename), True)
+        mapping = ''
+        sp = label_basename.split('_')
+        if len(sp) >= 3 and sp[2] == 'special source':
+            mapping = 'MASTER_FILE_1/Sliced Dialogue/Special source'
+            assert sp[0] == 'fim'
+            specifier = ''.join(re.findall(r'\d+', sp[1]))[1:]
+            specifier = 's' + specifier[0] + 'e' + str(int(specifier[1:]))
+            mapping += '/' + specifier
+            return mapping, specifier, True
+        elif len(sp) >= 3 and sp[2] == 'outtakes':
+            mapping = 'MASTER_FILE_1/Sliced Dialogue/Special source/Outtakes'
+            specifier = ''.join(re.findall(r'\d+', sp[1]))[1:]
+            mapping += '/' + specifier + ' outtakes'
+            return mapping, specifier, True
+        elif sp[0] == 'fim':
+            mapping = 'MASTER_FILE_1/Sliced Dialogue/FiM/'
+            specifier = ''.join(re.findall(r'\d+', sp[1]))[1:] 
+            se_specifier = 's' + specifier[0] + 'e' + str(int(specifier[1:]))
+            mapping += 'S' + specifier[0] + '/s' + specifier[0] + 'e' + str(int(specifier[1:]))
+            return mapping, se_specifier, False
+
+    #def specifier_to_num(sp):
+        #return sp[1], sp[3:]
+
+    # TODO:
+    # We need the original file path (i.e. the "current best" version as decided by Clipper)
+    # And we need to be able to find the associated episode, grab the center channel
+    def generate_fim_episodes_labels_index(
+            labels_dir = SLICED_DIALOGUE + "/Label files",
+            master_file_1 = 'D:/MLP_Samples/AIData/Master file',
+            master_file_2 = 'D:/MEGASyncDownloads/Master file 2',
+            max_noise = 1,
+            ignore_text = False,
+            no_parse = False,
+            audio_input_format = '.flac',
+            force_character = '',
+            emotions : list = []):
+            index = {}
+            special_episodes = set()
+            for (root,_,files) in os.walk(labels_dir):
+                for f in tqdm(files, desc="Label files"):
+                    # 'Other' not in scope for now
+                    if 'Other' in root:
+                        continue
+                    # Process fim episodes only
+                    if 'fim' not in f or 's' not in f:
+                        continue
+                    # Ignore original/izo text lists, we just care about overall
+                    if f.endswith('_original.txt') or f.endswith('_izo.txt') or f.endswith('_unmix.txt'):
+                        continue
+                    # If a special source version exists, prefer that
+                    f_basename = f.removesuffix('.txt')
+                    special_source_path = os.path.join(root, f_basename+'_special source.txt')
+                    if os.path.exists(special_source_path):
+                        continue
+
+                    mapping, specifier, special_source = PPPDataset.label_mapping(f_basename)
+                    if special_source:
+                        special_episodes.add(specifier)
+                    placeholder_mapping = mapping
+                    mapping = mapping.replace('MASTER_FILE_1', master_file_1)
+                    mapping = mapping.replace('MASTER_FILE_2', master_file_2)
+                    assert os.path.exists(mapping) or (specifier in special_episodes), mapping
+
+                    index[specifier] = {
+                        'season': specifier[1],
+                        'episode': specifier[3:],
+                        'lines': []
+                    }
+                    with open(os.path.join(root,f)) as f2:
+                        line = f2.readline()
+                        while line:
+                            sp = [x.strip() for x in line.split('\t')]
+                            sig = sp[2]
+                            sig = sig.replace('?','_')
+                            parse = PPPDataset.character_parse(sig)
+                            filepath = os.path.join(mapping, sig+'.flac')
+                            placeholder_filepath = os.path.join(placeholder_mapping, sig+'.flac')
+                            assert(os.path.exists(filepath), filepath)
+                            index[specifier]['lines'].append({
+                                'ts': sp[0],
+                                'te': sp[1],
+                                'orig_file': placeholder_filepath.replace('\\','/'),
+                                'parse': parse
+                            })
+                            line = f2.readline()
+            return index
+
     def all_dialogue_paths(self):
         paths = []
         for char,files in self.file_dict.items():
@@ -164,19 +308,24 @@ class PPPDataset:
         min_audio_ms = 0
         max_audio_ms = 0
         total_audio_ms = 0
+        char_dict = {}
         for char,files in self.file_dict.items():
             first_file = files[0]
             audio = AudioSegment.from_file(first_file['file'])
             min_audio_ms = len(audio)
+            char_audio_ms = 0
             for f in tqdm(files, "Files for character "+char):
                 audio = AudioSegment.from_file(f['file'])
                 audio_length_ms = len(audio)
                 min_audio_ms = min(min_audio_ms, audio_length_ms)
                 max_audio_ms = max(max_audio_ms, audio_length_ms)
+                char_audio_ms += audio_length_ms
                 total_audio_ms += audio_length_ms
-            print(f"Character: {char} ({len(files)} lines)\n"
+            print(f"Character: {char} ({len(files)} lines {char_audio_ms/1000} s)\n"
                 f"Min: {min_audio_ms/1000} s, Max: {max_audio_ms/1000} s\n",
                 f"Total: {total_audio_ms/1000} s")
+            char_dict[char] = char_audio_ms/1000
+        return char_dict
 
     def __getitem__(self, c):
         return self.file_dict[c]
@@ -651,13 +800,16 @@ class PPPDataset:
 #    'Applejack', 'Celestia', 'Luna', 'Cadance', 'Apple Bloom', 'Sweetie Belle',
 #    'Scootaloo', 'Starlight', 'Trixie', 'Spike', 'Big Mac', 'Zecora',
 #    'Cozy Glow', 'Cheerilee', 'Shining Armor', 'Discord', 'Diamond Tiara',
-#    'Gilda', 'Chrysalis', 'Tirek', 'Flim', 'Flam']).styletts2_portable(
-#            'D:/MLP_Samples/AIData/StyleTTS2Omnidata',
-#            'D:/MLP_Samples/AIData/StyleTTS2Omnidata/train_list.txt',
-#            'D:/MLP_Samples/AIData/StyleTTS2Omnidata/val_list.txt')
+#    'Gilda', 'Chrysalis', 'Tirek', 'Flim', 'Flam']).styletts2(
+#            'D:/MLP_Samples/AIData/StyleTTS2Omnidata48k',
+#            'D:/MLP_Samples/AIData/StyleTTS2Omnidata48k/train_list.txt',
+#            'D:/MLP_Samples/AIData/StyleTTS2Omnidata48k/val_list.txt',
+#            sr=48000)
 #PPPDataset.collect(
 #    ['Twilight', 'Rainbow', 'Pinkie', 'Fluttershy', 'Rarity', 'Applejack']).gpt_sovits(
 #        'D:/Code/GPT-SoVITS-beta/GPT-SoVITS/mane6_ft',
 #        'D:/Code/GPT-SoVITS-beta/GPT-SoVITS/mane6_ft.list',
 #    )
+idx = PPPDataset.generate_fim_episodes_labels_index()
+print(idx['s1e1']['lines'][0])
 #print("Done")
